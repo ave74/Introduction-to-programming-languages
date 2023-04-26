9f1751f01c0868419bfe6cad05c479d45de5772a
Anatolii Verkienko <ave1974@internet.ru>
2023-04-26 23:28:38 +0400

    Seminar 01 Home work

--- Seminar01/Program.cs
+++ Seminar01/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+//Задача 1
+//Напишите программу, которая на вход принимает число и выдаёт его квадрат
+Console.Write("Input your number: ");
+int num=Convert.ToInt32(Console.ReadLine());
+int result=num*num;
+Console.WriteLine("Your number is " + num + ". Square of this number is " + result);
+
+
+//Задача 3
+//Напишите программу, которая на вход принимает 2 числа и проверяет, является ли первое число квадратом второго
+Console.Clear();
+Console.Write("Input 1st number: ");
+int num1=Convert.ToInt32(Console.ReadLine());
+Console.Write("Input 2nd number: ");
+int num2=Convert.ToInt32(Console.ReadLine());
+//int result=num2*num2;
+if(num1==num2*num2)
+{
+    Console.Write("Your first number is a square of the second number");
+}
+else
+{
+    Console.Write("Your first number is NOT a square number of the second number");
+}
+*/
+
+//Задача 5
+//Напишите программу, которая на вход принимает одно число N, а на выходе показывает все целые числа от -N до N
+
+Console.Clear();
---
+++ Seminar01_HomeWork/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+//Задача 2
+//Напишите программу, которая на вход принимает 2 числа и выдаёт, какое число больше, а какое меньше
+
+Console.Clear();
+Console.Write("Input 1st number: ");
+int num1=Convert.ToInt32(Console.ReadLine());
+Console.Write("Input 2nd number: ");
+int num2=Convert.ToInt32(Console.ReadLine());
+
+if(num1>num2)
+{
+    Console.WriteLine($"Your first number is {num1}, second number is {num2}. First number is bigger than second one.");
+}
+else
+{
+    if(num1==num2)
+    {
+       Console.WriteLine($"Your first number is {num1} and second number is {num2}. These numbers are equal."); 
+    }
+    else
+    {
+    Console.WriteLine($"Your first number is {num1}, second number is {num2}. Second number is bigger than first one.");
+    }
+}
+
+//Задача 4
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+Console.Write("Input 1st number: ");
+int num1=Convert.ToInt32(Console.ReadLine());
+Console.Write("Input 2nd number: ");
+int num2=Convert.ToInt32(Console.ReadLine());
+Console.Write("Input 3rd number: ");
+int num3=Convert.ToInt32(Console.ReadLine());
+
+int max=num1;
+
+if(num2>num1 && num2>num3) max=num2;
+if(num3>num2 && num3>num1) max=num3;
+
+Console.WriteLine($"Maximum number is {max}");
+
+//Задача 6
+//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+//(делится ли оно на 2 без остатка)
+
+Console.Clear();
+Console.Write("Input your number: ");
+int num=Convert.ToInt32(Console.ReadLine());
+
+if(num%2==0)
+{
+    Console.WriteLine("Your number is even number.");
+}
+else
+{
+    Console.WriteLine("Your number is odd number.");
+}
+
+//Задача 8
+//Напишите программу, которая на вход принимает число N, а на выходе показывает все чётные числа от 1 до N
+
+Console.Clear();
+Console.Write("Input positive number: ");
+int num=Convert.ToInt32(Console.ReadLine());
+
+int count=2;
+
+Console.WriteLine("Even numbers from 1 until " + num + " are: ");
+
+while(count<=num)
+{
+    if(count%2==0) Console.Write(count + " ");
+    count++;
+}
+*/
