600f8e0ca0f71073db8bd002e4ff6529949cddcd
Anatolii Verkienko <ave1974@internet.ru>
2023-04-30 19:18:45 +0400

    Seminar 2 homework

---
+++ Seminar02/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+//Задание 1
+//Напишите программу, которая получает на вход двузначное число и показывает наибольшую цифру числа.
+
+int Digits(int number)
+{
+    //1. выделить цифры числа
+    int ed = number % 10;
+    int dec = number / 10;
+    //2. сравнить цифры
+    if (ed>dec)
+    {
+        return ed;
+    }
+    else
+    {
+        return dec;
+    }
+    //3. сообщить результат
+}
+
+Console.Clear();
+Console.Write("Input number: ");
+int user_number=Convert.ToInt32(Console.ReadLine());
+
+if (user_number>9 && user_number<100)
+{
+    int res=Digits(user_number);
+    Console.WriteLine($"The biggest digit is {res}");
+}
+else
+{
+    Console.WriteLine("Imposible value ");
+}
+
+
+//Задача 1 (невозвратный метод)
+
+void Digit1 (int number)
+{
+    int ed = number % 10;
+    int dec = number / 10;
+    if (ed>dec)
+    {
+    Console.WriteLine("The biggest digit is " + ed);
+    }
+    else
+    {
+        Console.WriteLine("The biggest digit is " + dec);
+    }
+}
+
+Console.Clear();
+Console.Write("Input number: ");
+int user_number=Convert.ToInt32(Console.ReadLine());
+
+if (user_number>9 && user_number<100)
+{
+       Digit1(user_number);
+}
+else
+{
+    Console.WriteLine("Imposible value ");
+}
+*/
+
+//Задача 2
+//Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
---
+++ Seminar02_Homework/Program.cs
@@ -0,0 +1,87 @@
+﻿/*
+//Задача 10
+//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+void Digit1(int number)
+{
+    int dec = number / 10;
+    int second_digit = dec % 10;
+    Console.WriteLine("The second digit is " + second_digit);
+}
+
+Console.Clear();
+Console.Write("Input 3 digit number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+
+if (user_number > 99 && user_number < 1000 || user_number > -1000 && user_number < -99)
+{
+    Digit1(Math.Abs(user_number));
+}
+else
+{
+    Console.WriteLine("Imposible value.");    
+}
+*/
+/*
+//Задача 13
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+void ThirdNumber(int user_number)
+{
+    int temp=user_number;
+    int num=user_number;
+    int digit=0;
+    
+    while(temp>0)
+    {
+        temp=temp/10;
+        digit++;
+    }
+    Console.WriteLine($"You have {digit} digits in your number.");
+    if(digit<3)
+    {
+        Console.WriteLine("Your number is below 100.");
+    }
+    else
+    {
+        Console.WriteLine("The third digit is " + (num/(Convert.ToInt32(Math.Pow(10, digit-3))))%10);
+    }
+}
+
+Console.Clear();
+Console.Write("Input any number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+
+user_number=Math.Abs(user_number);
+ThirdNumber(user_number);
+*/
+/*
+//Задача 15
+//Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+
+void Day(int week)
+{
+    if (week>5)
+    {
+        Console.WriteLine("This is a weekend.");
+    }
+    else
+    {
+        Console.WriteLine("This is a working day.");
+    }
+}
+
+Console.Clear();
+Console.Write("Input the number of the day of the week: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+
+if (user_number >= 1 && user_number <= 7)
+{
+    Day(user_number);
+}
+else
+{
+    Console.WriteLine("Imposible value.");    
+}
+*/
