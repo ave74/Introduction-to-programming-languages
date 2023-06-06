2823b04052d10420829b31ab603818cb6be7d13a
Anatolii Verkienko <ave1974@internet.ru>
2023-06-06 21:36:05 +0400

    Added Seminar 9 homework

---
+++ Seminar09/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N
+/*
+void ShowNumbers (int numbers)
+{
+    if (numbers>1) ShowNumbers(numbers-1);
+    Console.Write(numbers+" ");
+}
+ShowNumbers(5);
+*/
+//Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр
+/*
+int SumDigits (int num)
+{
+    if (num<0) num*=-1;
+    if (num>0) return SumDigits(num/10)+num%10;
+    else return 0
+}
+
+int res = SumDigits(987);
+Console.Write (res);
+*/
+
+//Напишите программу, которая на вход принимает два числа А и В,
+//и возводит число А в целую степень В с помощью рекурсии
+
+double FindPower (int a, int b)
+{
+    if (b<0) return FindPower(a, b+1) /a;
+    if (b>0) return FindPower(a, b-1) * a;
+    else return 1.0;
+}
+
+double res = FindPower(2, 3);
+Console.Write (res);
---
+++ Seminar09_Homework/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача 64. Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+/*
+Console.Clear();
+void ShowNumbers(int numbers)
+{
+    Console.Write(numbers + " ");
+    if (numbers > 1) ShowNumbers(numbers - 1);
+}
+
+ShowNumbers(10);
+*/
+
+//Задача 66. Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+//в промежутке от M до N.
+/*
+Console.Clear();
+int SumDigits(int m, int n)
+{
+    if (m == n) return m;
+    if (m < n) return n + SumDigits(m, n - 1);
+    return n + SumDigits(m, n + 1);           
+}
+
+int res = SumDigits(2, 6);
+Console.Write(res);
+*/
+
+//Задача 68. Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+/*
+Console.Clear();
+int Akk(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return Akk(m - 1, 1);
+    else return Akk(m - 1, Akk(m, n - 1));
+}
+
+int res = Akk(5, 0);
+Console.Write(res);
+*/
